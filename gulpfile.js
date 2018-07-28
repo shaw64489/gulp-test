7d944d44b2f50f8830203b07743d40f1a701eddb
Chris Shaw <shaw64489@yahoo.com>
2018-07-28 15:00:59 -0400

    minify-autoprefixer-concat-error-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,52 @@ var gulp = require("gulp");
 var uglify = require("gulp-uglify");
 //load in live reload library
 var livereload = require("gulp-livereload");
+//load in concat plugin
+var concat = require("gulp-concat");
+//load in minify plugin
+var minifyCss = require("gulp-minify-css");
+//load in autoprefixer plugin
+var autoprefixer = require("gulp-autoprefixer");
+//load in plumber plugin
+var plumber = require("gulp-plumber");
 
 //file paths
+var DIST_PATH = "public/dist";
 var SCRIPTS_PATH = "public/scripts/**/*.js";
+var CSS_PATH = "public/css/**/*.css";
 
 // Styles
 //create new task - two args - string name and function to run
 // when task is executed
 gulp.task("styles", () => {
   console.log("starting styles task");
+
+  //use concat plugin to return gulp src
+  return (
+    gulp
+      //load reset first - so order is right
+      .src(["public/css/reset.css", CSS_PATH])
+      //plumber - tell plumber to check for errors and handle
+      //pass anon function - called when error happens
+      .pipe(
+        plumber(function(err) {
+          console.log("Styles task error");
+          console.log(err);
+          //stop running rest of processes but keep gulp up
+          this.emit("end");
+        })
+      )
+      //prefix our styles
+      .pipe(autoprefixer())
+      //call concat and pass name of final file - combined version
+      .pipe(concat("styles.css"))
+      //minify
+      .pipe(minifyCss())
+      //put them back into the project to be processed
+      //save into public folder
+      .pipe(gulp.dest(DIST_PATH))
+      .pipe(livereload())
+  );
 });
 
 // Scripts
@@ -29,7 +66,7 @@ gulp.task("scripts", () => {
       .pipe(uglify())
       //put them back into the project to be processed
       //save into public folder
-      .pipe(gulp.dest("public/dist"))
+      .pipe(gulp.dest(DIST_PATH))
       .pipe(livereload())
   );
 });
@@ -55,4 +92,5 @@ gulp.task("watch", () => {
   //pass path of files we're watching
   //array of tasks we want to run
   gulp.watch(SCRIPTS_PATH, ["scripts"]);
+  gulp.watch(CSS_PATH, ["styles"]);
 });
