10704b9a687faf7ebaf53b279452062ce6d13abf
Chris Shaw <shaw64489@yahoo.com>
2018-07-29 12:05:55 -0400

    sass-finish styles task - modify scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,18 @@ var minifyCss = require("gulp-minify-css");
 var autoprefixer = require("gulp-autoprefixer");
 //load in plumber plugin
 var plumber = require("gulp-plumber");
+//load in sourcemaps plugin
+var sourcemaps = require("gulp-sourcemaps");
+//load in sass plugin
+var sass = require("gulp-sass");
 
 //file paths
 var DIST_PATH = "public/dist";
 var SCRIPTS_PATH = "public/scripts/**/*.js";
 var CSS_PATH = "public/css/**/*.css";
 
-// Styles
+/*
+// Styles for plain CSS
 //create new task - two args - string name and function to run
 // when task is executed
 gulp.task("styles", () => {
@@ -39,12 +44,58 @@ gulp.task("styles", () => {
           this.emit("end");
         })
       )
+      //kick off sourcemaps process
+      .pipe(sourcemaps.init())
       //prefix our styles
       .pipe(autoprefixer())
       //call concat and pass name of final file - combined version
       .pipe(concat("styles.css"))
       //minify
       .pipe(minifyCss())
+      //writes the source map in the new file
+      .pipe(sourcemaps.write())
+      //put them back into the project to be processed
+      //save into public folder
+      .pipe(gulp.dest(DIST_PATH))
+      .pipe(livereload())
+  );
+});
+*/
+
+// Styles - for Sass
+//create new task - two args - string name and function to run
+// when task is executed
+gulp.task("styles", () => {
+  console.log("starting styles task");
+
+  //use concat plugin to return gulp src
+  return (
+    gulp
+      //only include main styles.scss file
+      .src("public/scss/styles.scss")
+      //plumber - tell plumber to check for errors and handle
+      //pass anon function - called when error happens
+      .pipe(
+        plumber(function(err) {
+          console.log("Styles task error");
+          console.log(err);
+          //stop running rest of processes but keep gulp up
+          this.emit("end");
+        })
+      )
+      //kick off sourcemaps process
+      .pipe(sourcemaps.init())
+      //prefix our styles
+      .pipe(autoprefixer())
+      //dont need concat or minify because thats built into sass plugin
+      //call sass
+      .pipe(
+        sass({
+          outputStyle: "compressed"
+        })
+      )
+      //writes the source map in the new file
+      .pipe(sourcemaps.write())
       //put them back into the project to be processed
       //save into public folder
       .pipe(gulp.dest(DIST_PATH))
@@ -62,8 +113,24 @@ gulp.task("scripts", () => {
   return (
     gulp
       .src(SCRIPTS_PATH)
+      //plumber - tell plumber to check for errors and handle
+      //pass anon function - called when error happens
+      .pipe(
+        plumber(function(err) {
+          console.log("Scripts task error");
+          console.log(err);
+          //stop running rest of processes but keep gulp up
+          this.emit("end");
+        })
+      )
+      //kick off sourcemaps process
+      .pipe(sourcemaps.init())
       //call uglify
       .pipe(uglify())
+      //add concat step
+      .pipe(concat("scripts.js"))
+      //writes the source map in the new file
+      .pipe(sourcemaps.write())
       //put them back into the project to be processed
       //save into public folder
       .pipe(gulp.dest(DIST_PATH))
@@ -92,5 +159,7 @@ gulp.task("watch", () => {
   //pass path of files we're watching
   //array of tasks we want to run
   gulp.watch(SCRIPTS_PATH, ["scripts"]);
-  gulp.watch(CSS_PATH, ["styles"]);
+  //gulp.watch(CSS_PATH, ["styles"]);
+  //update for sass
+  gulp.watch("public/scss/**/*.scss", ["styles"]);
 });
--- public/dist/main.js
+++
@@ -1,1 +0,0 @@
-var name="Chris";document.write("Hello "+name+"!");
